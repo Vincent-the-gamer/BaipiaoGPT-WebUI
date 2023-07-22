69426a08a375a2a0ef0aea6b72aeb3126be737d0
vincent-the-gamer <1127620453@qq.com>
2023-07-22 13:19:33 +0800

    feature: 根据时间段自动设置默认主题

--- store/useThemeStore.ts
+++ store/useThemeStore.ts
@@ -1,9 +1,18 @@
 import { defineStore } from "pinia";
+import dayjs from "dayjs";
+
+// 根据不同时间段设置默认主题，18点到次日早上7点是晚上，其他时候是白天
+const currentHour: number = dayjs().hour()
+
+function getCurrentTheme(): string{
+    if(currentHour >= 18 && currentHour <= 7) return "dark"
+    else return "light"
+}
 
 const useThemeStore = defineStore("themeStore", {
     state: () => {
         return {
-            theme: "light" as string
+            theme: getCurrentTheme() as string
         }
     },
     actions: {
